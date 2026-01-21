910d441ceaef5d5b2e8c30a77044a9ba06e26b47
falishaarn <93347663+falishaarn@users.noreply.github.com>
2026-01-21 21:39:19 +0700

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -60,7 +60,7 @@ with st.sidebar:
     st.title("Credit Collectibility Predictor")
     st.markdown("---")
     if st.button("🏠 Home"): set_menu("🏠 Home")
-    if st.button("🔍 Prediksi & Output"): set_menu("🔍 Prediction & Output")
+    if st.button("🔍 Prediction & Output"): set_menu("🔍 Prediction & Output")
     if st.button("📈 Analytics Dashboard"): set_menu("📈 Analytics Dashboard")
     if st.button("🧠 Feature Insights"): set_menu("🧠 Feature Insights")
     st.markdown("---")
