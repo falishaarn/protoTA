8b68ba8c43955b11d3aef4303d58bc6aa2f0d731
falishaarn <93347663+falishaarn@users.noreply.github.com>
2026-01-24 19:28:33 +0700

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -163,7 +163,7 @@ elif menu == "Model Training":
                     
                     y = df_new['Collectibility'] - 1 
 
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
                     
                     smt = SMOTETomek(random_state=42)
                     X_train_res, y_train_res = smt.fit_resample(X_train, y_train)
